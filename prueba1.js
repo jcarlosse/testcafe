e77ba6abfdfd2ea21aaa7402f87e0ebd4aca652d
Juan Carlos Suárez <suarezpepsi@hotmail.com>
2021-04-27 00:24:00 -0500

    anotaciones

--- prueba1.js
+++ prueba1.js
@@ -1,8 +1,16 @@
 import { Selector } from 'testcafe';
 
 fixture('Mis primeras pruebas en testcafe')
-    .page('https://devexpress.github.io/testcafe/example/')
+    .page('https://devexpress.github.io/testcafe/example/');
 
-test('Probar que la pagina abra correctamente', async t => {
-    await 35
-});
+//test('nombre de prueba', funcion asincrona {})
+
+test('Probar que la pagina se abra', async t => {
+
+    await t //sin  ; porque sigue siendo la misma declaracion
+        .typeText(Selector('#developer-name'), 'Carlos')
+        .expect(Selector('#developer-name').value).equal('Calos')
+}//final de la función asincrona
+)//fin de Test 
+    ;
+
