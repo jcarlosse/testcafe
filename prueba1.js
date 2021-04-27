da33616f63006f34ea2ebff7e3234d406e64fc08
Juan Carlos Suárez <suarezpepsi@hotmail.com>
2021-04-26 23:39:07 -0500

    Primer commit

---
+++ prueba1.js
@@ -0,0 +1,8 @@
+import { Selector } from 'testcafe';
+
+fixture('Mis primeras pruebas en testcafe')
+    .page('https://devexpress.github.io/testcafe/example/')
+
+test('Probar que la pagina abra correctamente', async t => {
+    await 35
+});
